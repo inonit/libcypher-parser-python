b1d7240fbba77065b08ae821c6b4a959d4cd1638
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-04-03 23:33:02 +0200

    Added parse_statement to __all__

--- libcypher_parser/__init__.py
+++ libcypher_parser/__init__.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from libcypher_parser.parser import parse_statement
+
 __title__ = 'libcypher-parser-python'
 __version__ = '0.0.1'
 __author__ = 'Rolf Haavard Blindheim'
 __license__ = 'Apache License 2.0'
 
 VERSION = __version__
+
+__all__ = [
+    'parse_statement'
+]
